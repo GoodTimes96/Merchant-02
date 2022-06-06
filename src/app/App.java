a1bd0c2085af2f2d4a3f8b8678949fa0197f49b1
Alex <crashedice93@gmail.com>
2022-06-06 12:36:11 +0300

    Add output method with surname

--- src/app/App.java
+++ src/app/App.java
@@ -26,6 +26,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter merchant name: ");
         merchantName = scanner.nextLine();
+        System.out.print("Enter merchant surname: ");
         System.out.print("Enter merchant phone: ");
         phone = scanner.nextLine();
         System.out.print("Enter merchant email: ");
